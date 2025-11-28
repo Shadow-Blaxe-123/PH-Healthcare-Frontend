2db38b406d8f134576c6b51940333850c7a3c73d
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-11-28 21:37:06 +0600

    cookie setting done

--- src/services/auth/loginUser.ts
+++ src/services/auth/loginUser.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
 
+import { parse } from "cookie";
+import { cookies } from "next/headers";
 import z from "zod";
 
 const loginValidationSchema = z.object({
@@ -15,6 +17,8 @@ export default async function loginUser(
   formData: FormData
 ): Promise<any> {
   try {
+    let accessTokenObject: null | any = null;
+    let refreshTokenObject: null | any = null;
     const loginData = {
       password: formData.get("password"),
       email: formData.get("email"),
@@ -39,10 +43,48 @@ export default async function loginUser(
       headers: {
         "Content-Type": "application/json",
       },
-    }).then((res) => res.json());
+    });
     console.log("res:", res);
 
-    return res;
+    const result = await res.json();
+
+    const setCookieHeaders = res.headers.getSetCookie();
+
+    if (setCookieHeaders && setCookieHeaders.length > 0) {
+      setCookieHeaders.forEach((cookie: string) => {
+        const parsedCookie = parse(cookie);
+        if (parsedCookie["accessToken"]) {
+          accessTokenObject = parsedCookie;
+        }
+        if (parsedCookie["refreshToken"]) {
+          refreshTokenObject = parsedCookie;
+        }
+      });
+    } else {
+      throw new Error("No Set-Cookie header found");
+    }
+    if (!accessTokenObject) {
+      throw new Error("Tokens not found in cookies");
+    }
+    if (!refreshTokenObject) {
+      throw new Error("Tokens not found in cookies");
+    }
+    const cookieStore = await cookies();
+
+    cookieStore.set("accessToken", accessTokenObject.accessToken, {
+      secure: true,
+      httpOnly: true,
+      maxAge: parseInt(accessTokenObject.MaxAge),
+      path: accessTokenObject.Path || "/",
+    });
+    cookieStore.set("refreshToken", refreshTokenObject.refreshToken, {
+      secure: true,
+      httpOnly: true,
+      maxAge: parseInt(refreshTokenObject.MaxAge),
+      path: refreshTokenObject.Path || "/",
+    });
+
+    return result;
   } catch (error) {
     console.log(error);
     return { error: "Login failed" };
