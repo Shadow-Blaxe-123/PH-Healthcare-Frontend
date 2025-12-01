c178580e8fc7f2e7926e21ef8dffad7522ea2a4e
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-12-01 23:22:02 +0600

    Module 68-7

--- src/components/modules/Dashboard/DashboardNavbar.tsx
+++ src/components/modules/Dashboard/DashboardNavbar.tsx
@@ -1,8 +1,9 @@
-function DashboardNavbar() {
-  return (
-    <div>
-      <h1>This is DashboardNavbar component.</h1>
-    </div>
-  );
+import { getUserInfo } from "@/services/auth/getUserInfo";
+import DashboardNavbarContent from "./DashboardNavbarContent";
+import { UserInfo } from "@/types/user.interface";
+
+async function DashboardNavbar() {
+  const userInfo = await getUserInfo();
+  return <DashboardNavbarContent userInfo={userInfo as UserInfo} />;
 }
 export default DashboardNavbar;
